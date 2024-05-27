4d24c83ca36ccebb569795865ae8de7a34fc00e2
zhekabaila <zhekabaila@gmail.com>
2024-05-27 08:33:56 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import Router from './Router'
+import Home from './pages/Home'
+import TourFrame from './pages/TourFrame'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 const App = () => {
-  return <Router />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tours/main" element={<TourFrame />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
